973e9ec2c150aeb13546d82b805ce0f9f29c54ee
zv20 <zv@icloud.com>
2022-04-28 15:37:38 -0400

    Initial Commit

---
+++ iosApiClient/iosApiClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApiClientApp.swift
+//  iosApiClient
+//
+//  Created by Zhivko Vanev on 28.04.22.
+//
+
+import SwiftUI
+
+@main
+struct iosApiClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
