cf890d7bfd73f29879c7cfb5fb919315d3708e87
zv20 <zhivkovanev@icloud.com>
2022-04-29 10:14:22 -0400

    Update Xcode

--- iosApiClient/Models/Customer.swift
+++ iosApiClient/Models/Customer.swift
@@ -1,13 +1,16 @@
 //
 //  Customer.swift
 //  iosApiClient
-// 
+// integreat me
 //  Created by Zhivko Vanev on 28.04.22.
-//
+// hdhdh 
 
 import Foundation
+import Fluent
+
 
 struct Customer: Identifiable, Codable {
     let id: UUID?
     var name: String
 }
+
