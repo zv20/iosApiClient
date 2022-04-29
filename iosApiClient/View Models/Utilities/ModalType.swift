80d5198bea405659443ff23c7162443d3ccb5440
zv20 <zv@icloud.com>
2022-04-29 06:45:54 -0400

    Create ModalType.swift
    ee

---
+++ iosApiClient/View Models/Utilities/ModalType.swift
@@ -0,0 +1,20 @@
+//
+//  ModalType.swift
+//  iosApiClient
+//
+//  Created by Zhivko Vanev on 28.04.22.
+//
+
+import Foundation
+
+enum ModalType: Identifiable {
+    var id: String {
+        switch self {
+        case .add: return "add"
+        case .update: return "update"
+        }
+    }
+    
+    case add
+    case update(Customer)
+}
